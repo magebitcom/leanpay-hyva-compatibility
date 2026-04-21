8204fbc86f7faca19702853bbb9264c0269aee2c
Mairis Ķimenis <mairis.kimenis@magebit.com>
2026-04-21 17:28:26 +0300

    LMM-122 Add support for dynamic prices

--- ViewModel/Installments.php
+++ ViewModel/Installments.php
@@ -3,9 +3,13 @@ declare(strict_types=1);
 
 namespace Leanpay\PaymentHyva\ViewModel;
 
+use Hyva\Theme\ViewModel\CurrentProduct;
 use Leanpay\Payment\Block\Installment\Pricing\Render\TemplatePriceBox;
+use Magento\Catalog\Block\Product\View;
 use Magento\Checkout\Model\Session;
+use Magento\Framework\Serialize\SerializerInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Magento\Framework\View\LayoutInterface;
 
 class Installments implements ArgumentInterface
 {
@@ -17,10 +21,17 @@ class Installments implements ArgumentInterface
     /**
      * @param TemplatePriceBox $templatePriceBox
      * @param Session $checkoutSession
+     * @param LayoutInterface $layout
+     * @param SerializerInterface $serializer
+     * @param CurrentProduct $currentProduct
      */
     public function __construct(
         private readonly TemplatePriceBox $templatePriceBox,
         private readonly Session $checkoutSession,
+        private readonly LayoutInterface $layout,
+        private readonly SerializerInterface $serializer,
+        private readonly CurrentProduct $currentProduct,
+
     ) {
     }
 
@@ -35,6 +46,44 @@ class Installments implements ArgumentInterface
             (float)$this->checkoutSession->getQuote()->getGrandTotal(),
             true
         );
+    }
+
+    /**
+     * Check if current page is PDP
+     *
+     * @return bool
+     */
+    public function isProductPage(): bool
+    {
+        return in_array('catalog_product_view', $this->layout->getUpdate()->getHandles());
+    }
+
+    /**
+     * Get current product ID
+     *
+     * @return int
+     */
+    public function getProductId(): int
+    {
+        return (int)$this->currentProduct->get()?->getId();
+    }
+
+    /**
+     * Get installment value map
+     *
+     * @return string
+     */
+    public function getInstallmentMap(): string
+    {
+        if (!$this->isProductPage()) {
+            return '{}';
+        }
+
+        $block = $this->layout->createBlock(View::class);
+
+        $installmentMap = $this->serializer->unserialize($block->getJsonConfig());
+
+        return (string)$this->serializer->serialize($installmentMap['installmentHtmlMap'] ?? []);
     }
 
     /**
